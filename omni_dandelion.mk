e343b4f0f2bc921e38dd25c5ebbc8eee6ce39542
Sushrut1101 <guptasushrut@gmail.com>
2022-01-03 17:01:09 +0530

    dandelion: Initial Tree
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ omni_dandelion.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dandelion
+PRODUCT_NAME := omni_dandelion
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 9A
+PRODUCT_MANUFACTURER := xiaomi
