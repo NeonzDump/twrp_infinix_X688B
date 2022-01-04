8e93f56e6e951a29d27619293948ca64157df822
Sushrut1101 <guptasushrut@gmail.com>
2022-01-04 16:12:34 +0530

    dandelion: Build Fastbootd
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -16,3 +16,9 @@
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+	fastbootd \
+	android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.0-impl-mock.recovery
