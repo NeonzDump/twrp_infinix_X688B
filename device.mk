41e888cb5a6b99038b0f6a22edadc4de666481d5
Sushrut1101 <guptasushrut@gmail.com>
2022-01-04 16:13:41 +0530

    dandelion: Move Some flags to device.mk
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -22,3 +22,16 @@ PRODUCT_PACKAGES += \
 	fastbootd \
 	android.hardware.fastboot@1.0-impl-mock \
 	android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+	libkeymaster4 \
+	libpuresoftkeymasterdevice \
+	ashmemd_aidl_interface-cpp \
+	libashmemd_client
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
