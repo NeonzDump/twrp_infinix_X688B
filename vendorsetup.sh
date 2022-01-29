d48c37760ee0d1a824d5ca660ec9d0e9f7fb627d
Sushrut1101 <guptasushrut@gmail.com>
2022-01-29 17:45:01 +0530

    garden: Do Not Permit the User to Disable the Navigation Bar
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -82,6 +82,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_STATUS_INDENT_LEFT=48
 	export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
