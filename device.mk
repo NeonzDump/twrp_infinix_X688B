ea3ba55d3dea888b42bef76f9e77845e5d2b70e0
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2022-04-14 00:13:03 +0530

    garden: Fix Fastbootd
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -21,9 +21,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-	fastbootd \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery
+	android.hardware.fastboot@1.0-impl-mock
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
