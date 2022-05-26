42d19725a418e1ed4cd27bad8da6e0ff761dedb3
Sushrut1101 <guptasushrut@gmail.com>
2022-05-26 17:27:50 +0530

    garden: Update Libraries Stuff
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -26,12 +26,8 @@ PRODUCT_PACKAGES += \
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
 	libkeymaster4 \
-	libpuresoftkeymasterdevice \
-	ashmemd_aidl_interface-cpp \
-	libashmemd_client
+	libpuresoftkeymasterdevice
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+RECOVERY_LIBRARY_SOURCE_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
+	$(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
